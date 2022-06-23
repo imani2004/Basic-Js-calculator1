a8732224bf7a5c446154e54d82e622e696677d67
Dorna <dorna.imani943@gmail.com>
2022-06-23 11:32:37 +0300

    First commit

---
+++ calc.js
@@ -0,0 +1,15 @@
+const num1=parseInt(prompt("Enter the first number "))
+var operation=prompt("Enter the operation to be done")
+const num2=parseInt(prompt("Enter the second number"))
+if(operation== '+'){
+     alert(num1 + num2)
+}
+else if (operation== '-'){
+    alert(num1-num2)
+}
+else if(operation== '*'){
+    alert(num1*num2)
+}
+else if(operation== '/'){
+    alert(num1/num2)
+}
